ff6fe67be3e7b5596f36bfae72733dabbb25fba3
Muhammad Mazhar <34936746+MuhammadMazhar313@users.noreply.github.com>
2023-04-11 15:28:59 +0500

    BaseResponse added

--- routes/services/users.js
+++ routes/services/users.js
@@ -1,6 +1,8 @@
 const db = require('./db');
 const helper = require('../../helper');
 const config = require('../../config');
+// Import the responseApi.js
+const { success, error, validation } = require("./BaseResponse");
 
 async function getMultiple(page = 1) {
     const offset = helper.getOffset(page, config.listPerPage);
