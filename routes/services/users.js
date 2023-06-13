26e1ec4ab0747e1b21f54e854085825ebd7f96ba
Muhammad Mazhar <34936746+MuhammadMazhar313@users.noreply.github.com>
2023-06-13 16:06:10 +0500

    updated description

--- routes/services/users.js
+++ routes/services/users.js
@@ -56,7 +56,7 @@ async function login(User) {
             response = errorObj("Incorrect UserName or password.", {}, 200)
         }
     } catch (error) {
-        response = errorObj(" Something went wrong, please enter correct user name or password ", {}, 200)
+        response = errorObj(" Something went wrong, Please enter correct user name or password ", {}, 200)
         // return 
     }
 
